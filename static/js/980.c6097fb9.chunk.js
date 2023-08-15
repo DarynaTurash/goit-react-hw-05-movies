4626a99acdfa261fe86a21b85b36e94f9d7174cc
DarynaTurash <120209050+DarynaTurash@users.noreply.github.com>
2023-08-15 14:07:24 +0000

    Deploying to gh-pages from @ DarynaTurash/goit-react-hw-05-movies@8ea5192840751f2c6e8ecd30b98e8dc257f30727 🚀

---
+++ static/js/980.c6097fb9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[980],{980:function(e,t,r){r.r(t);var n=r(689),i=r(87),c=r(184);t.default=function(e){var t=e.list,r=(0,n.TH)();return(0,c.jsx)("section",{children:(0,c.jsx)("ul",{children:t.map((function(e){return(0,c.jsx)("li",{children:(0,c.jsx)(i.rU,{to:"/movies/".concat(e.id),state:{from:r},children:e.original_title||e.name})},e.id)}))})})}}}]);
+//# sourceMappingURL=980.c6097fb9.chunk.js.map
